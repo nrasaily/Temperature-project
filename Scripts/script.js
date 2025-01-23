5de392194432b83653f9de3123f2c518446697d1
Nar Rasaily <nareshsathi9@gmail.com>
2025-01-22 22:58:04 -0500

    Creating Variables

---
+++ Scripts/script.js
@@ -0,0 +1,81 @@
+console.log("Variables");
+//keyword nameVAE= value;
+
+
+//string
+var firstName = "Nar";
+var lastName = "Rasaily";
+
+
+// numerical value
+
+let myAge=35;
+let myHeight=169;
+
+
+//Boolean value true / false
+let isStudent = true;
+
+
+console.log("My Name is: " + firstName + ", I am "  + myAge + "years old, my size is " + myHeight);
+
+let num1 = 10;
+let num2 = 2;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+
+console.log("the sum is: " + sum);
+console.log("the sub is: " + sub);
+console.log("the mul is: " + mul);
+
+
+
+let area;
+let radius = 10;
+const pi = 3.1416
+
+radius=5;
+
+
+area = pi * radius * radius;
+
+
+console.log("The area of a cirsle with a radius of " + radius + " value is the " + area);
+
+
+
+let integerNumber = 0;
+
+integerNumber = 1625;
+
+console.log("Value of integer number is " + integerNumber);
+integerNumber=375;
+
+//alert(integerNumber + 125)
+
+
+//myAge=prompt("Enter user age")
+
+
+
+//alert("thank you for your answer");
+//alert("you are " + myAge + "years old");
+
+let aVariable=20;
+
+let otherVariable=10;
+
+console.log(aVariable,otherVariable);
+
+aVariable=30;
+otherVariable=40;
+
+console.log(aVariable,otherVariable);
+
+
+
+const weekDays = 7;
+console.log(weekDays)
+
