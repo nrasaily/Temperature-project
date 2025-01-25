67d1470a2d729e8465954d06a7c8daeb7ee74cc1
Nar Rasaily <nareshsathi79@gmail.com>
2025-01-25 01:00:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+console.log("Variables");
+//keyword nameVAE= value;
+
+
+//string
+var firstName = "Nar";
+var lastName = "Rasaily";
+
+
+// numerical value
+
+let myAge=35;
+let myHeight=169;
+
+
+//Boolean value true / false
+let isStudent = true;
+
+
+console.log("My Name is: " + firstName + ", I am "  + myAge + "years old, my size is " + myHeight);
+
+let num1 = 10;
+let num2 = 2;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+
+console.log("the sum is: " + sum);
+console.log("the sub is: " + sub);
+console.log("the mul is: " + mul);
+
+
+
+let area;
+let radius = 10;
+const pi = 3.1416
+
+radius=5;
+
+
+area = pi * radius * radius;
+
+
+console.log("The area of a cirsle with a radius of " + radius + " value is the " + area);
+
+
+
+let integerNumber = 0;
+
+integerNumber = 1625;
+
+console.log("Value of integer number is " + integerNumber);
+integerNumber=375;
+
+//alert(integerNumber + 125)
+
+
+//myAge=prompt("Enter user age")
+
+
+
+//alert("thank you for your answer");
+//alert("you are " + myAge + "years old");
+
+let aVariable=20;
+
+let otherVariable=10;
+
+console.log(aVariable,otherVariable);
+
+aVariable=30;
+otherVariable=40;
+
+console.log(aVariable,otherVariable);
+
+
+
+const weekDays = 7;
+console.log(weekDays)
+
+let charactor = "Mulla Nasuridhin";
+let object = "Well";
+let place = "in the Village ?";
+let price = "$100";
+let action = "take water"
+
+
+console.log("--- Funny Story---");
+
+console.log("A man asked " + charactor, + " Can I buy your " + object + place );
+console.log( charactor + " sold it but later refused to let the man " + action );
+console.log(" The man complained, I bought the " + object + " at " + price,  + " why can't I " + action);
+console.log(charactor + " replied, you only bought the " + object + " not the water."  );
+
+
+const charactor1 = "Mulla";
+let charactor2 = " The Teacher";
+let charactor3 = "donkey";
+let setting = "tree outside the school";
+
+console.log("---Story No. 2----")
+console.log(charactor1 + " tied his " + charactor3 + " to a" + setting);
+console.log(charactor2 + " angrily said, why bring your " + charactor3 + " here ?");
+console.log(charactor1 + " replied, well I thought he could learn something new."); 
+console.log(charactor2 + " shouted, he is a " + charactor3 + " ! he will never learn!.");
+console.log(charactor1 + " laughed, Exactly why I thought he'd fit in this class.");
+console.log(charactor2 + " never asked.");
